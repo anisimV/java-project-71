10c15342896d301f55455c3dfc1ed3d20f37ab98
Анисии <117117637+anisimV@users.noreply.github.com>
2025-05-06 21:38:28 +0300

    Comparison of flat files (JSON)

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,13 +6,48 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
         Map<String, Object> data1 = getData(filePath1);
         Map<String, Object> data2 = getData(filePath2);
 
-        return "File1: " + data1 + "\nFile2: " + data2;
+        // создаем отсортированое множество всей ключей из обоиъ файлов
+
+        Set<String> allKeys = new TreeSet<>();
+        allKeys.addAll(data1.keySet()); // добавляем ключи из первого файла
+        allKeys.addAll(data2.keySet()); // добавляем ключи из второго файла
+
+        // строим строку-результат с помощью StringBuilder
+        StringBuilder result = new StringBuilder("{\n");
+
+        // проходим по каждому ключу в алфавитном порядке
+
+        for (String key : allKeys) {
+            Object val1 = data1.get(key); // значение из первого файла (может быть null
+            Object val2 = data2.get(key); // значение из первого файла (может быть null
+
+            // если ключ отсутствует во втором файле - он был удален
+            if (!data2.containsKey(key)) {
+                result.append("  - ").append(key).append(": ").append(val1).append("\n");
+                // если ключ отсутствует в первом файле - он был добавлен
+            } else if (!data1.containsKey(key)) {
+                result.append("  + ").append(key).append(": ").append(val2).append("\n");
+                // если ключи равны - ключ не изменяется
+            } else if (Objects.equals(val1, val2)) {
+                result.append("    ").append(key).append(": ").append(val1).append("\n");
+            } else {
+                // если ключ есть в обоих файлах, но значения разные - ключ был изменен
+                result.append("  - ").append(key).append(": ").append(val1).append("\n");
+                result.append("  + ").append(key).append(": ").append(val2).append("\n");
+            }
+        }
+
+        result.append("}");
+        return result.toString();
     }
 
     private static Map<String, Object> getData(String resourcePath) throws Exception {
@@ -24,4 +59,3 @@ public class Differ {
         return mapper.readValue(content, Map.class);
     }
 }
-
